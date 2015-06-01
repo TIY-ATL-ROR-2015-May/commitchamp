905623b54c4f8df22ee2118849f12c3cb5a5a2bc
Brit Butler <redline6561@gmail.com>
2015-06-01 14:38:30 -0400

    Some untested commits from review.

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -9,10 +9,53 @@ require 'commitchamp/github'
 module Commitchamp
   class App
     def initialize
-      @github = Github.new
+      if ENV['OAUTH_TOKEN']
+        token = ENV['OAUTH_TOKEN']
+      else
+        token = get_auth_token
+      end
+      @github = Github.new(token)
+    end
+
+    def prompt(question, validator)
+      puts question
+      input = gets.chomp
+      until input =~ validator
+        puts "Sorry, didn't understand that answer."
+        puts question
+        input = gets.chomp
+      end
+      input
+    end
+
+    def get_auth_token
+      prompt("Please enter your personal access token for Github: ",
+            /^[0-9a-f]{40}$/)
+    end
+
+    def import_contributors(repo_name)
+      repo = Repo.first_or_create(name: repo_name)
+      results = @github.get_contributors('redline6561', repo_name)
+      results.each do |contributor|
+        user = User.first_or_create(name: contributor['author']['login'])
+        lines_added = contributor['weeks'].map { |x| x['a'] }.sum
+        lines_deleted = contributor['weeks'].map{ |x| x['d'] }.sum
+        commits_made = contributor['weeks'].map { |x| x['c'] }.sum
+        # Contribution.create(user_id: user.id,
+        #                      lines_added: lines_added,
+        #                      lines_deleted: lines_deleted,
+        #                      commits_made: commits_made,
+        #                      repo_id: repo.id)
+
+
+        user.contributions.create(lines_added: lines_added,
+                                  lines_deleted: lines_deleted,
+                                  commits_made: commits_made,
+                                  repo_id: repo.id)
+      end
     end
   end
 end
 
-# app = Commitchamp::App.new
-# binding.pry
+app = Commitchamp::App.new
+binding.pry
--- lib/commitchamp/github.rb
+++ lib/commitchamp/github.rb
@@ -4,5 +4,26 @@ module Commitchamp
   class Github
     include HTTParty
     base_uri "https://api.github.com"
+
+    def initialize(access_token)
+      @headers = {
+        "Authorization" => "token #{access_token}",
+        "User-Agent"    => "HTTParty"
+      }
+    end
+
+    def get_user(username)
+      self.class.get("/users/#{username}",
+                     headers: @headers)
+    end
+
+    def get_contributors(owner, repo)
+      self.class.get("/repos/#{owner}/#{repo}/stats/contributors",
+                     headers: @headers)
+    end
+
+    # def extract_stat(contributor, stat_name)
+    #   contributor['weeks'].map { |x| x[stat_name] }.sum
+    # end
   end
 end
